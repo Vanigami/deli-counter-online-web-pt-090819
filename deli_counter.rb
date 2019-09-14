74ef6c5d25e240ee5e8be77ea0d35cf7b8e7ff45
Vanna Winland <vgwinland@gmail.com>
2019-09-14 16:36:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-     katz_deli = "The line is currently:"
+      current_line = "The line is currently:"
   katz_deli.each.with_index(1) do |customer, i|
     katz_deli << " #{i}. #{customer}"
   end
