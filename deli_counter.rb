402cca631e491923051c2b3a6d752651ff729f6a
Vanna Winland <vgwinland@gmail.com>
2019-09-14 16:35:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,16 +3,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
+    
   end 
 end
 
 def take_a_number(katz_deli, customer)
-  katz_deli = "The line is currently:"
-  katz_deli.each.with_index(1) do |customer, i|
-    katz_deli << " #{i}. #{customer}"
-  end
-  puts katz_deli
-  
+ 
 end
 
 def now_serving
